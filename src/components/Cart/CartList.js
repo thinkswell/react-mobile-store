4096ba9992f85784d04ef93de4d19278ba07df48
Krishna Vishwakarma <krishna23712@gmail.com>
2020-03-30 01:12:38 +0530

    modal of product added

---
+++ src/components/Cart/CartList.js
@@ -0,0 +1,17 @@
+import React from "react";
+import CartItem from "./CartItem";
+
+export default function CartList({ value }) {
+  const { cart } = value;
+  console.log(value);
+  console.log(cart);
+
+  return (
+    <div className="container-fluid">
+      {cart.map(item => (
+        <CartItem key={item.id} item={item} value={value} />
+      ))}
+      
+    </div>
+  );
+}
---
+++ src/components/ProductList.js
@@ -0,0 +1,27 @@
+import React, { Component } from "react";
+import Product from "./Product";
+import Title from "./Title";
+import { ProductConsumer } from "../context";
+
+export default class ProductList extends Component {
+  render() {
+    return (
+      <React.Fragment>
+        <div className="py-5">
+          <div className="container">
+            <Title name="Our" title="Product" />
+            <div className="row">
+              <ProductConsumer>
+                {value => {
+                  return value.products.map(product => (
+                    <Product key={product.id} product={product} />
+                  ));
+                }}
+              </ProductConsumer>
+            </div>
+          </div>
+        </div>
+      </React.Fragment>
+    );
+  }
+}
